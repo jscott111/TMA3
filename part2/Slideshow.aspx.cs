8a129a9f500234f81e65621fc779607fbfff7d1b
jscott111 <33634599+jscott111@users.noreply.github.com>
2021-11-24 11:32:58 -0400

    Update Slideshow.aspx.cs

--- part2/Slideshow.aspx.cs
+++ part2/Slideshow.aspx.cs
@@ -8,57 +8,64 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Services;
 
 namespace Project
 {
-    public partial class Slideshow : System.Web.UI.Page
+    public partial class Slideshow : Page
     {
-        public void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            messages.Text += "Program Started ";
-            Task.Run(async () =>
+            int counter = 0;
+            SqlConnection con = new SqlConnection("Server=tcp:jscott11.database.windows.net,1433;Initial Catalog=photos;Persist Security Info=False;User ID=jscott11;Password=3557321Joh--;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            byte[] imageData;
+            string imageUrl = "http://tma3.azurewebsites.net/part2/Images/CRW_5523.jpg";
+            int height = (Request.Browser.ScreenPixelsHeight) * 2 - 400;
+
+            imageData = new System.Net.WebClient().DownloadData(imageUrl);
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(imageData);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(stream);
+            img.Save(Server.MapPath("img.bmp"));
+            SqlCommand command = new SqlCommand("SELECT name, caption, url FROM [dbo].[pic]", con);
+            con.Open();
+            command.ExecuteNonQuery();
+            using (SqlDataReader reader = command.ExecuteReader())
             {
-                SqlConnection con = new SqlConnection("Server=tcp:jscott11.database.windows.net,1433;Initial Catalog=photos;Persist Security Info=False;User ID=jscott11;Password=3557321Joh--;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-                byte[] imageData;
-                string imageUrl = "http://tma3.azurewebsites.net/part2/Images/CRW_5523.jpg";
-                //int height = (Request.Browser.ScreenPixelsHeight) * 2 - 320;
-                int height = 500;
-
-                imageData = new System.Net.WebClient().DownloadData(imageUrl);
-                System.IO.MemoryStream stream = new System.IO.MemoryStream(imageData);
-                System.Drawing.Image img = System.Drawing.Image.FromStream(stream);
-                img.Save(Server.MapPath("img.bmp"));
-                SqlCommand command = new SqlCommand("SELECT name, caption, url FROM [dbo].[pic]", con);
-                con.Open();
-                command.ExecuteNonQuery();
-                using (SqlDataReader reader = command.ExecuteReader())
+                while (reader.Read())
                 {
-                    while (reader.Read())
-                    {
-                        picture.ImageUrl = reader[2].ToString();
-                        imageUrl = "http://tma3.azurewebsites.net/part2/" + picture.ImageUrl;
-                        imageData = new System.Net.WebClient().DownloadData(imageUrl);
-                        stream = new System.IO.MemoryStream(imageData);
-                        img = System.Drawing.Image.FromStream(stream);
-                        img.Save(Server.MapPath("img.bmp"));
-                        float ratio = (float)img.Height / (float)img.Width;
-                        picture.Width = new Unit(height / ratio);
-                        picture.Height = new Unit(height);
-                        await Task.Delay(1000);
-                        print();
-                    }
+                    Image picture = new Image();
+                        
+                    imageUrl = "http://tma3.azurewebsites.net/part2/Images/" + reader[2].ToString();
+                    imageData = new System.Net.WebClient().DownloadData(imageUrl);
+                    stream = new System.IO.MemoryStream(imageData); 
+                    img = System.Drawing.Image.FromStream(stream);
+                    img.Save(Server.MapPath("img.bmp"));
+                    float ratio = (float)img.Height / (float)img.Width;
+                    picture.Width = new Unit(height / ratio);
+                    picture.Height = new Unit(height);
+
+                    picture.Style.Add("display", "none");
+                    picture.ImageUrl = imageUrl;
+                    picture.ID = counter.ToString();
+                    pictures.Controls.Add(picture);
+
+                    Label label = new Label();
+                    label.Text = reader[1].ToString();
+                    label.ID = "cap" + counter;
+                    label.Style.Add("display", "none");
+                    caption.Controls.Add(label);
+
+                    counter++;
                 }
-                con.Close();
-            });
-
-            messages.Text += "Program Finished ";
-        }
-
-        void print()
-        {
-            messages.Text += "Picture Loaded ";
+            }
+            con.Close();
+            counter--;
+            numPics.Value = counter.ToString();
         }
     }
 }
+
+
+
 
 
