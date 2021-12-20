8127bdc32cc94628df7469864d125aaca95469af
jscott111 <33634599+jscott111@users.noreply.github.com>
2021-12-20 15:10:07 -0400

    Update Feedback.aspx.cs

--- part3/Feedback.aspx.cs
+++ part3/Feedback.aspx.cs
@@ -21,7 +21,8 @@ namespace Store
             var smtpClient = new SmtpClient("smtp.gmail.com")
             {
                 Port = 587,
-                Credentials = new NetworkCredential("john5sco717@gmail.com", "wlqebhbzuvtkfqxg")
+                Credentials = new NetworkCredential("john5sco717@gmail.com", "wlqebhbzuvtkfqxg"),
+                EnableSsl = true
             };
 
             smtpClient.Send("john5sco717@gmail.com", "johnascott14@gmail.com", "Thanks for the feedback", "Test");
