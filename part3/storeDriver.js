bd7cade62b8733a85564a0f0d8759d891bf36106
jscott111 <33634599+jscott111@users.noreply.github.com>
2021-12-20 14:29:07 -0400

    Add files via upload

---
+++ part3/storeDriver.js
@@ -0,0 +1,15 @@
+
+function componentList(id) {
+    document.writeln("<form id='form' action='ComponentList.aspx' runat='server'>");
+    document.writeln("    <input type='hidden' id='system' name='system' value=" + id + ">");
+    document.writeln("</form>");
+
+    document.getElementById("form").submit();
+}
+
+function goHome() {
+    document.writeln("<form id='form' action='Home.aspx' runat='server'>");
+    document.writeln("</form>");
+
+    document.getElementById("form").submit();
+}
