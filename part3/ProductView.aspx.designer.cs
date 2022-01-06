361917885289a0c584a4e29b03bd65f329ac6da6
jscott111 <33634599+jscott111@users.noreply.github.com>
2022-01-05 20:25:04 -0400

    Create ProductView.aspx.designer.cs

---
+++ part3/ProductView.aspx.designer.cs
@@ -0,0 +1,11 @@
+namespace Store
+{
+
+
+    public partial class ProductView
+    {
+        protected global::System.Web.UI.WebControls.Label title;
+
+        protected global::System.Web.UI.WebControls.Panel product;
+    }
+}
