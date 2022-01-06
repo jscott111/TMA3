8178f9aa643fc5808ec47b336da0208682026f0e
jscott111 <33634599+jscott111@users.noreply.github.com>
2022-01-05 20:26:28 -0400

    Update storeDriver.js

--- part3/storeDriver.js
+++ part3/storeDriver.js
@@ -13,3 +13,11 @@ function goHome() {
 
     document.getElementById("form").submit();
 }
+
+function productView(id) {
+    document.writeln("<form id='form' action='ProductView.aspx' runat='server'>");
+    document.writeln("    <input type='hidden' id='system' name='system' value=" + id + ">");
+    document.writeln("</form>");
+
+    document.getElementById("form").submit();
+}
