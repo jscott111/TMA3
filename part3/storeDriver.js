0137ef1475ad17de253c19e17818f7cf7c89882a
jscott111 <33634599+jscott111@users.noreply.github.com>
2022-01-30 13:41:18 -0400

    Update storeDriver.js

--- part3/storeDriver.js
+++ part3/storeDriver.js
@@ -1,7 +1,28 @@
 
 function componentList(id) {
     document.writeln("<form id='form' action='ComponentList.aspx' runat='server'>");
+    document.writeln("    <input type='hidden' id='part' name='part' value=" + id + ">");
+    document.writeln("</form>");
+
+    document.getElementById("form").submit();
+}
+
+function swapComponent(id, system) {
+    document.writeln("<form id='form' action='ComponentList.aspx' runat='server'>");
+    document.writeln("    <input type='hidden' id='system' name='system' value=" + system + ">");
+    document.writeln("    <input type='hidden' id='part' name='part' value=" + id + ">");
+    document.writeln("    <input type='hidden' id='swap' name='swap' value='true'>");
+    document.writeln("</form>");
+
+    document.getElementById("form").submit();
+}
+
+function productView(id, partID, part, swap) {
+    document.writeln("<form id='form' action='ProductView.aspx' runat='server'>");
     document.writeln("    <input type='hidden' id='system' name='system' value=" + id + ">");
+    document.writeln("    <input type='hidden' id='partID' name='partID' value=" + partID + ">");
+    document.writeln("    <input type='hidden' id='part' name='part' value=" + part + ">");
+    document.writeln("    <input type='hidden' id='swap' name='swap' value=" + swap + ">");
     document.writeln("</form>");
 
     document.getElementById("form").submit();
@@ -14,9 +35,10 @@ function goHome() {
     document.getElementById("form").submit();
 }
 
-function productView(id) {
-    document.writeln("<form id='form' action='ProductView.aspx' runat='server'>");
-    document.writeln("    <input type='hidden' id='system' name='system' value=" + id + ">");
+function addToCart(ip, system) {
+    document.writeln("<form id='form' action='Cart.aspx' runat='server'>");
+    document.writeln("    <input type='hidden' id='ip' name='ip' value=" + ip + ">");
+    document.writeln("    <input type='hidden' id='system' name='system' value=" + system + ">");
     document.writeln("</form>");
 
     document.getElementById("form").submit();
