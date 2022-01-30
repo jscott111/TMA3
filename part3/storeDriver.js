e044ca68014594bad41edbec128246668243783e
jscott111 <33634599+jscott111@users.noreply.github.com>
2022-01-30 14:59:56 -0400

    Update storeDriver.js

--- part3/storeDriver.js
+++ part3/storeDriver.js
@@ -39,6 +39,7 @@ function addToCart(ip, system) {
     document.writeln("<form id='form' action='Cart.aspx' runat='server'>");
     document.writeln("    <input type='hidden' id='ip' name='ip' value=" + ip + ">");
     document.writeln("    <input type='hidden' id='system' name='system' value=" + system + ">");
+    document.writeln("    <input type='hidden' id='action' name='action' value='add'>");
     document.writeln("</form>");
 
     document.getElementById("form").submit();
