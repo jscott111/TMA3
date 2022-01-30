589d07f1e121e9300b5b9c8b2332cc3723472b14
jscott111 <33634599+jscott111@users.noreply.github.com>
2022-01-30 14:47:08 -0400

    Update Cart.aspx.cs

--- part3/Cart.aspx.cs
+++ part3/Cart.aspx.cs
@@ -3,6 +3,7 @@ using System.Web.UI;
 using System.Data.SqlClient;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
+using System.Web;
 
 namespace Store
 {
@@ -10,14 +11,31 @@ namespace Store
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(Request.QueryString["action"] == "add")
+            SqlConnection con = new SqlConnection("Server=tcp:jscott11.database.windows.net,1433;Initial Catalog=store;Persist Security Info=False;User ID=jscott11;Password=3557321Joh--;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            SqlCommand command;
+            if (Request.QueryString["action"] == "add")
             {
-                title.Text = Request.QueryString["ip"] + ": " + Request.QueryString["system"];
+                command = new SqlCommand("INSERT INTO [dbo].[cart] (ip, system) VALUES (" + Request.QueryString["ip"] + ", " + Request.QueryString["system"] + ")", con);
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
             }
-            if(Request.QueryString["action"] == "view")
+            if(Request.QueryString["action"] == "delete")
             {
                 title.Text = Request.QueryString["ip"];
             }
+
+            command = new SqlCommand("SELECT system FROM [dbo].[cart] WHERE ip = " + Request.QueryString["ip"], con);
+            con.Open();
+            command.ExecuteNonQuery();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    title.Text = reader[0].ToString();
+                }
+            }
+            con.Close();
         }
     }
 }
